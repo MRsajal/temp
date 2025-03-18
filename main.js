9f931980a98400c3af15508503e54e7dda8f9352
Sajal <mostafizurrahamnsajal8@gmail.com>
2025-03-18 12:36:35 +0600

    updated

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+import { Client, Databases } from "node-appwrite";
+
+const PROJECT_ID = process.env.PROJECT_ID;
+const DB_ID = process.env.DB_ID;
+const COLLECTION_ID = process.env.COLLECTION_ID;
 export default async ({ req, res, log, error }) => {
-  return res.send("hello world");
+  const client = new Client();
+  client.setEndpoint("https://cloud.appwrite.io/v1").setProject(PROJECT_ID);
+
+  const db = new Databases(client);
+  if (req.method == "GET") {
+    const response = await db.listDocuments(DB_ID, COLLECTION_ID);
+    return res.json(response.documents);
+  }
 };
